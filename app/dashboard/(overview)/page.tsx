5724c76b4cdc57f41abc6e2c12d0b1bd9455e74d
rdietscht <rdietscht@gmail.com>
2024-12-14 18:35:35 -0500

    Optimizing load times by streaming slow component
    Fetching revenue data inside the revenue chart
    component and wrapping it around a Suspense
    component that includes a RevenueChartSkeleton
    fallback.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,15 +2,15 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '../../lib/data';
 import { fetchLatestInvoices } from '../../lib/data';
 import { fetchCardData } from '../../lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 
 export default async function Page() {
 
   // Fetch all the necessary data using the data.ts Server Component.
-  const revenue = await fetchRevenue (); // Revenue
 
   const latestInvoices = await fetchLatestInvoices (); // Latest invoices
 
@@ -37,7 +37,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
