ee2468a77650412283595be00100a0c7b6b8a017
rdietscht <rdietscht@gmail.com>
2024-12-19 13:04:30 -0500

    Added form validation to edit form and UI errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -40,13 +40,27 @@ export async function deleteInvoice (id: string)
     }
 }
 
-export async function updateInvoice (id: string, formData: FormData)
+export async function updateInvoice (id: string, prevState: State, formData: FormData)
 {
-    const { customerId, amount, status } = UpdateInvoice.parse ({
+    const validatedFields = UpdateInvoice.safeParse ({
         customerId: formData.get ('customerId'),
         amount: formData.get ('amount'),
         status: formData.get ('status'),
     });
+
+    console.log (validatedFields); // DEBUGGING!
+
+    // EARLY EXIT - One/more of the fields were invalid.
+    if (!validatedFields.success)
+    {
+        return {
+            errors: validatedFields.error.flatten ().fieldErrors,
+            message: 'Missing Fields. Failed to update Invoice.',
+        }
+    }
+
+    // Extract the valid data from validatedFields.
+    const { customerId, amount, status } = validatedFields.data;
 
     const amountInCents = amount * 100;
 
