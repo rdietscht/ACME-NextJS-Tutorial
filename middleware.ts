839d74e547ec6a755bed7ceee8d2fe78497e4935
rdietscht <rdietscht@gmail.com>
2024-12-20 13:44:56 -0500

    Fixed 'Module Not Found' issue within middleware
    For some reason, the Next.js service was unable to
    locate the NextAuth module when trying to import
    it via 'next-auth' from the project's root.
    
    In order to make it work, I simply gave the
    import code a relative path, after which pnpm was
    able to compile and run the code quickly.

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth from "./node_modules/.pnpm/next-auth@5.0.0-beta.25_next@15.1.0_react-dom@19.0.0-rc-cd22717c-20241013_react@19.0.0-rc-cd2_vpnyommcfiul5rcc26jgvsqwl4/node_modules/next-auth";
 import { authConfig } from "./auth.config";
 
 export default NextAuth (authConfig).auth;
