0fdff83799dd121b4150ed7b176ec0a3f02c2472
rdietscht <rdietscht@gmail.com>
2024-12-21 17:02:15 -0500

    Adding metadata title templates to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,10 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata =
 {
-  title: 'Acme Dashboard',
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL ('https://next-learn-dashboard.vercel.sh'),
 };
