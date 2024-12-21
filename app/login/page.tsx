90f69f1ee64896c876d3293c505ea469e1e94247
rdietscht <rdietscht@gmail.com>
2024-12-21 17:11:20 -0500

    Added more specific page titles for app

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,11 @@
 import AcmeLogo from "../ui/acme-logo";
 import LoginForm from "../ui/login-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata =
+{
+    title: 'Login',
+};
 
 export default function LoginPage ()
 {
